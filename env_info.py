5b65792cea2cd3f6898d762439b1a8e4ddb20177
Yeonguk Kim <rudghk1437@naver.com>
2022-12-06 16:56:24 +0900

    env_info.py 지우기

--- env_info.py
+++
@@ -1,9 +0,0 @@
-# web_scraper/settings.py 설정값
-SECRET_KEY = 'django-insecure-m-=xx5y_$5nwrhj4w^9&*##%!nbxxuf86y_xrnum7)w55@+sc5'
-DEBUG = True
-ALLOWED_HOSTS = []
-
-# telegram 정보 : web_scraper/scripts/scraper.py
-
-TLGM_BOT_API = "5904227464:AAGmNea-orR3Ab3UqqdJRKaxgz71uX4HeqM"
-chat_id = "5933023298"
