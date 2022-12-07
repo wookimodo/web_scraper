ef64854a35029e2566deb82c53f01e2d9aa8c66c
Yeonguk Kim <rudghk1437@naver.com>
2022-12-07 17:26:31 +0900

    Rest Framework API를 통한 데이터 확인

--- web_scraper/urls.py
+++ web_scraper/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hotdeal.views import index
+from rest_framework import routers
+from hotdeal import views
+
+router = routers.DefaultRouter()
+router.register("deals", views.DealViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('api/', include(router.urls)),
 ]
